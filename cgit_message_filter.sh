fa40e63131b0c323181108eefcdbebe8f81ef38f
Timo Paulssen <timonator@perpetuum-immobile.de>
2010-08-18 15:33:06 +0200

    uness put in a single line, the var would expire, leading to a bad URL.

--- cgit_message_filter.sh
+++ cgit_message_filter.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-ROUNDUP_BASE='http://example.com/roundup/'
-perl -pe 's{(issue(\d+))}{<a href="$ENV{ROUNDUP_BASE}/issue$2">$1</a>}ig'
+ROUNDUP_BASE='http://example.com/roundup/' perl -pe 's{(issue(\d+))}{<a href="$ENV{ROUNDUP_BASE}/issue$2">$1</a>}ig'
